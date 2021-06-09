18dcfba28d1875aca998993a3caf5e7391a5d5b1
Tracy Harvey <63746813+flowxyno@users.noreply.github.com>
2021-06-08 23:34:58 -0700

    Contact.htm / contac.css
    Submission of V2 of Contact.htm pending suggested edits from other team members

--- thanks.php
+++ thanks.php
@@ -1,83 +1,59 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<title>Thank you!!</title>
+    <title>American Pizzas - Contact Request Receipt</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-<!--
+    <link href="base.css" rel="stylesheet" />
+    <link href="contact.css" rel="stylesheet" />
+    <script src="base.js"></script>
+    <!-- 
+        CIS 155 Team Project - Spring 2021
+            
+        Site Name: American Pizza
+        Team Name: Developer Dudes
+        Authors: Adam Rodriguez, Tracy Harvey, James Coolidge
+    -->
 
-S17 Tracy Harvey U08B
-
-What objectives are you demonstrating?
-
-- Usage of variables to store incoming form data
-- Usage of isset to check to see if there is any data to be stored
-- Useage of print to input the HTML code for the displaying the processed results of the incoming form data
-
--->
-
-<style>
-
-
-</style>
-
-</head>
+<!-- remove the inline style!! It just here to show how the page would look. (remove this comment before publishing)-->
 <body>
-<?php
+    <!-- The navigation menu list. -->
+    <nav id="nav_menu">
+    	<ul>
+            <!-- Move the 'current' class to whichever page this is. It will make that button stand out to show the user which page they're on. (remove this comment before publishing) -->
+            <li><a href="#"><img src="site_images/logo.jpg" alt="American Pizza"/></a></li>
+    		<li><a href="#">Home</a></li>
+    		<li><a href="#">Menu</a></li>
+            <li><a href="#">Locations</a></li>
+    		<li><a href="#">Contact</a></li>
+    	</ul>
+        <!-- lines under links -->
+        <div id="line_under_link1"></div>
+        <div id="line_under_link2"></div>
+    </nav>
+	<main>
+       
+        <?php
+        
+        if(isset($_POST['cname'])) { $cust = $_POST['cname']; } else { $cust = ""; }
+        if(isset($_POST['cmail'])) { $mail = $_POST['cmail']; } else { $mail = ""; }
+        if(isset($_POST['mess'])) { $mes = $_POST['mess']; } else { $mes = "";}
+            
+        print "
+            <div id='receipt'>
+                <h1>Thank you for your feed back it is very valuable to us.</h1>
+                <div id='inner'>
+                    <fieldset>
+                        <legend>Synopsis of Customer Feedback: </legend>
+                        <p><b>Customer Name: </b>$cust</p>
+                        <p><b>Customer Email: </b>$mail</p>
+                        <p><b>Customer Input: </b><br /> $mes </p>
+                    </fieldset>
+                </div
+            </div>
+        ";
 
-// Show errors, but not all...
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-// Get data from a GET or POST (change GET to POST for post)
-if(isset($_POST['cname'])) { $cust = $_POST['cname']; } else { $cust = ""; }
-if(isset($_POST['cmail'])) { $mail = $_POST['cmail']; } else { $mail = ""; }
-
-
-
-// Printing out the HTML tags and data needed to produce a readable receipt for the order
-print "
-        <p>Hello $cust</p><br />
-        <p>Your E-mail Address is: $mail</p>
-    ";
-
-//     // logic for printing items out on the receipt
-//     if ($size == "sm") {
-//         print "<pre>Size: Small                $5.00</pre>";
-//         $tot = $tot + 5; // adding the price of the item if it is selected to the running total of the bill
-//     }
-//     if ($size == "md") {
-//         print "<pre>Size: Medium              $10.00</pre>";
-//         $tot = $tot + 10; // adding the price of the item if it is selected to the running total of the bill
-//     }
-//     if ($size == "lg") {
-//         print "<pre>Size: Large               $15.00</pre>";
-//         $tot = $tot + 15; // adding the price of the item if it is selected to the running total of the bill
-//     }
-//     if ($pep) {
-//         print "<pre>Item: Pepperoni            $1.00</pre>";
-//         $tot = $tot + 1; // adding the price of the item if it is selected to the running total of the bill
-//     }
-//     if ($bcn) {
-//         print "<pre>Item: Bacon                $3.00</pre>";
-//         $tot = $tot + 3; // adding the price of the item if it is selected to the running total of the bill
-//     }
-//     if ($mush) {
-//         print "<pre>Item: Mushroom             $1.00</pre>";
-//         $tot = $tot + 1; // adding the price of the item if it is selected to the running total of the bill
-//     }
-//     if ($gp) {
-//         print "<pre>Item: Green Pepper         $1.00</pre>";
-//         $tot = $tot + 1; // adding the price of the item if it is selected to the running total of the bill
-//     }
-
-//     $ftot = number_format($tot, 2); // formatting the running total of the bill to 2 decimal place value for a clean visual for the total at the bottom of the receipt
-
-// print "<br />
-//        <pre>Total:                     $$ftot</pre><br />
-//        </p>
-//        </div>";
-?>
+        ?>
 </body>
 </html>
