4cb8d11baea4edc04840ae3945674bb4fac2c183
JamesProgramming <JamesCoolidge@student.olympic.edu>
2021-06-10 20:25:42 -0700

    update to menu styles & animation

--- menu-style.js
+++ menu-style.js
@@ -23,27 +23,52 @@ for (var i = 0; i < itemsElements.length; i++) {
                               <input type='text' value='1'/>\
                               <div><img src='site_images/plus.svg'/></div>";
     itemsElements[i].appendChild(itemQuantity);
+    
+    
+    var remove = document.createElement("img");
+    remove.setAttribute("src", "site_images/remove_shopping.svg");
+    remove.setAttribute("id", "remove");
+    itemsElements[i].insertAdjacentElement("afterbegin", remove);
+
+    var cart = document.createElement("img");
+    cart.setAttribute("src", "site_images/shopping_cart.svg");
+    cart.setAttribute("id", "cart");
+    itemsElements[i].insertAdjacentElement("afterbegin", cart);
+    
+    var add = document.createElement("img");
+    add.setAttribute("src", "site_images/add_shopping.svg");
+    add.setAttribute("id", "add");
+    itemsElements[i].insertAdjacentElement("afterbegin", add);
+
     itemsElements[i].addEventListener("click", function() {
-        var x = document.getElementsByClassName(orderForm[this.children[1].id].id)[0];
-        if (!(orderForm[this.children[1].id].checked)) {
+        var x = document.getElementsByClassName(orderForm[this.children[4].id].id)[0];
+        if (!(orderForm[this.children[4].id].checked)) {
             this.style.borderColor = "#ac5959";
             this.lastChild.setAttribute("style", "");
-            orderForm[this.children[1].id].checked = true;
+            orderForm[this.children[4].id].checked = true;
             this.lastChild.children[1].value = "1";
             x.value = 1;
+            itemHover(this, true);
         } else {
             this.style.borderColor = "";
             this.lastChild.setAttribute("style", "display:none;");
-            orderForm[this.children[1].id].checked = false;
+            orderForm[this.children[4].id].checked = false;
             this.lastChild.children[1].value = "0";
             x.value = 0;
+            itemHover(this, false);
         }
-        
+
+        this.lastChild.onmouseover = function(e) {
+            e.stopPropagation();
+            this.parentNode.children[2].style.display = "none";
+            this.parentNode.children[1].style.display = "block";
+        }
+
         this.lastChild.firstChild.onmouseover = function() {
             this.firstChild.style.filter = "drop-shadow(rgba(0,0,0,0.9) 2px 2px 2px)";
             this.onclick = function(e) {
                 e.stopPropagation();
-                if (Number(this.parentNode.children[1].value) > 0) {
+                if (Number(this.parentNode.children[1].value) > 1) {
                     this.parentNode.children[1].value = parseInt(this.parentNode.children[1].value) - 1;
                     x.value = Number(x.value) - 1;
                 }
@@ -95,4 +120,33 @@ for (var i = 0; i < itemsElements.length; i++) {
             }
         }
     });
+}
+
+function itemHover(divObject, add) {
+    if (add) {
+        divObject.children[0].style.display = "none";
+        divObject.children[2].style.display = "block";
+    } else {
+        divObject.children[0].style.display = "";  
+        divObject.children[2].style.display = ""; 
+    }
+    divObject.onmouseover = function() {
+        if (add) {
+            this.children[1].style.display = "none";
+            this.children[2].style.display = "block"; 
+        } else {
+            this.children[1].style.display = "";
+            this.children[2].style.display = ""; 
+        }
+    }
+
+    divObject.onmouseleave = function() {
+        if (add) {
+            this.children[1].style.display = "block";
+            this.children[2].style.display = "none";  
+        } else {
+            this.children[1].style.display = "";
+            this.children[2].style.display = ""; 
+        }
+    }
 }
