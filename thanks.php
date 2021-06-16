e9bd93534bcb2cad5d4e64cf04b739a2ac55f8ac
Tracy Harvey <63746813+flowxyno@users.noreply.github.com>
2021-06-15 23:12:45 -0700

    Update thanks.php

--- thanks.php
+++ thanks.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>American Pizzas - Contact Request Receipt</title>
+    <title>American Pizzas - Contact Receipt</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="base.css" rel="stylesheet" />
